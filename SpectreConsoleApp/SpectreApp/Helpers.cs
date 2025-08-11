c6f44277dbec050a6ebb21b920120dbb05d3be92
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-08-11 16:28:20 -0400

    Added JsonOutput, Fegilet output and Multiselect Input prompt

---
+++ SpectreConsoleApp/SpectreApp/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectreApp;
+
+public static class Helpers
+{
+    public static async Task<string> FetchApiData(string apiUrl)
+    {
+        using HttpClient client = new HttpClient();
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+            return string.Empty;
+        }
+    }
+}
--- SpectreConsoleApp/SpectreApp/Program.cs
+++ SpectreConsoleApp/SpectreApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Spectre.Console;
+using Spectre.Console.Json;
+using SpectreApp;
+using System.Text.Json.Nodes;
 
+#region Markup data console writing
 AnsiConsole.MarkupLine("[bold green]Hello, World![/]");
 AnsiConsole.MarkupLine("[bold red]This is a test.[/]");
 AnsiConsole.MarkupLine("[underline blue]This is a test.[/]");
@@ -10,7 +14,9 @@ AnsiConsole.MarkupLine("[bold green]Hello, World! 🌍[/]");
 AnsiConsole.MarkupLine("[bold red]This is a test. 🚀[/]");
 AnsiConsole.MarkupLine("[underline blue]This is a test. 🔵[/]");
 AnsiConsole.MarkupLine("[italic yellow]This is a test. 💛[/]");
+#endregion
 
+#region Table data console writing
 // Create a table
 var table = new Table();
 table.AddColumn("Column 1");
@@ -18,6 +24,67 @@ table.AddColumn("Column 2");
 table.AddRow("Row 1, Cell 1", "Row 1, Cell 2");
 table.AddRow("Row 2, Cell 1", "Row 2, Cell 2");
 AnsiConsole.Write(table);
+#endregion
 
-Console.ReadLine();
+//Console.ReadLine();
 AnsiConsole.Clear();
+
+#region FetchApiData and display in JsonText
+// Fetch API data example JsonPlaceholder
+string apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
+string data = await Helpers.FetchApiData(apiUrl);
+JsonText json = new JsonText(data);
+json.StringColor(Color.Yellow);
+json.ColonColor(Color.Red);
+
+AnsiConsole.Write(new Panel(json)
+                    .Header("Api Response")
+                    .Collapse()
+                    .BorderColor(Color.White)
+                    );
+#endregion
+
+AnsiConsole.Clear();
+#region Fegilet Text
+AnsiConsole.Write(new FigletText("Hello, Mani")
+    .LeftJustified()
+    .Color(Color.Red));
+
+FigletText figletText = new FigletText("Maran!");
+figletText.Justification = Justify.Center;
+figletText.Color = Color.Yellow;
+AnsiConsole.Write(figletText);
+#endregion
+
+#region Multiselect
+List<string> usualNames = [
+    "Foo Charis",
+    "Bar Alex",
+    "Baz Ben",
+    "Qux Pege",
+    "Quu Lubo"
+    ];
+
+List<string> familyNames = [
+    "Charis",
+    "Alex",
+    "Ben"
+    ];
+
+List<string> favoriteNames = AnsiConsole.Prompt(
+    new MultiSelectionPrompt<string>()
+    .Title("Which are your favorite placeholder names?")
+    .InstructionsText("Press <space> to toggle, <enter> to accept")
+    //.AddChoices(usualNames)
+    .AddChoiceGroup("Usual Names", usualNames)
+    .AddChoiceGroup("Family Names", familyNames)
+    );
+
+foreach(string name in favoriteNames)
+{
+    AnsiConsole.MarkupLine($"[Green]{name}[/]");
+}
+
+#endregion
+
+Console.Read();
